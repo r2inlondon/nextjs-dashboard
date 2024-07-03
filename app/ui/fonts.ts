e2642881a913602dda2c7849bd86026edd81b044
Arturo Quiroz <r2inlondon@gmail.com>
2024-07-03 17:46:45 +0100

    chapter 3 completed

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Roboto } from 'next/font/google';
+
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({ subsets: ['latin'], weight:['100' , '300' , '400' , '500' , '700' , '900'] });
