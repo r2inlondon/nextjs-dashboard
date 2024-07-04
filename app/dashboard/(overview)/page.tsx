2b6678c769c3377d35ec3e3a897cdfa104e8093c
Arturo Quiroz <r2inlondon@gmail.com>
2024-07-04 17:56:02 +0100

    chapter 9 completed

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from 'react';
+
+import { roboto } from '@/app/ui/fonts';
+
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { fetchCardData } from '@/app/lib/data';
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${roboto.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardWrapper />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
