1b9651523c68ed43b3e1e3c95db251a16c3e2f2f
Arturo Quiroz <r2inlondon@gmail.com>
2024-07-07 20:28:22 +0100

    chapter 16 completed

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,7 @@ import { roboto } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
 interface IProps {
   searchParams?: {
@@ -13,6 +14,10 @@ interface IProps {
     page?: string;
   };
 }
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page({ searchParams }: IProps) {
   const query = searchParams?.query || '';
